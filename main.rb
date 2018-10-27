0f2cd4afc56200a9acc5bc7bae25ccab5fcb1530
Kelvin <allecan52@gmail.com>
2018-10-27 16:52:01 -0600

    arreglando menu

--- main.rb
+++ main.rb
@@ -5,110 +5,13 @@ def limpiar_pantalla
   system('clear')
 end
 
-def buscar_libro(pila)
-  libro = pila[:tope]
-  print 'Ingrese el ISBN del Libro: '
-   isbn = gets.chomp
-  contador=1
-  a=0
-  while contador <= pila[:size]
-    if isbn == libro[:ISBN]
-      contador=pila[:size]+1
-      a=1
-    elsif libro [:siguiente]==nil&&libro[:ISBN]!=isbn
-      a=0
-    else
-      libro = libro[:siguiente]
-    end
-    contador+=1
-  end
-  if a==1
-    limpiar_pantalla
-    user_table = table do |t|
-      t.headings = 'ISBN', 'Nombre del Libro', 'Autor', 'Precio', 'Existencias'
-      t << [
-        libro[:ISBN],
-        libro[:nombre],
-        libro[:autor],
-        libro[:Precio],
-        libro[:existencias]
-      ]
-    end
-    puts user_table
-  else
-    limpiar_pantalla
-    puts 'El libro no se encuentra en el sistema'
-  end
-  gets()
-  end
-end
-
-venta ={
-  tope:nil,
-  max:20,
-  size:0
-  vacio:true,
-  final:nil
-}
-cola ={
-  vacio:true,
-  size:0,
-  max:5,
-  tope:nil,
-  final:nil
-}
-pila ={
-  tope:nil,
-  vacio:true,
-  size:0
-}
-begin
-  puts "\tBiblioteca de lirbros"
-  puts 'Listado de opciones: '
-  puts '1. Registro de Libros'
-  puts '2. Control de Ventas'
-  puts '3. Salir'
-  print 'Ingrese una opcion: '
-  opcion=gets.chomp
-  case opcion
-  when '1'
-    begin
-      limpiar_pantalla()
-      puts "\tRegistro de Libros"
-      puts 'Listado de opciones: '
-      puts '1. Registro de Nuevo Libro'
-      puts '2. Registro de Nuevo Autor'
-      puts '3. Buscar Libro'
-      puts '4. Buscar Autor'
-      puts '5. Lista de Libros'
-      puts '6. Lista de Autores'
-      puts '7. Salir'
-      print 'Ingrese una opcion: '
-      opcion1 = gets.chomp
-      case opcion1
-      when '1'
-      when '2'
-      when '3'
-        buscar_libro(pila)
-      when '4'
-      when '5'
-      when '6'
-      end
-    end while opcion1 != '7'
-  when '2'
-  end
-  limpiar_pantalla()
-end while opcion != '3'
-
-
-def listado_de_libros(pila)
+def lista_libros(pila)
   if pila[:tope] == nil
     puts 'No Hay Libros Ingresados O En Existencia'
   else
   tabla = Terminal::Table.new do |t|
     t.title = 'Listado de Libros'
     t.headings = ['ISB', 'NOMBRE', 'PRECIO','AUTOR','EXISTENCIAS']
-
     aux = pila[:tope]
     loop do
       t.add_row([
@@ -124,3 +27,116 @@ def listado_de_libros(pila)
       aux = aux[:siguiente]
     end
   end
+  puts tabla
+  gets
+end
+
+def buscar_libro(pila)
+  libro = pila[:tope]
+  print 'Ingrese el ISBN del Libro: '
+  isbn = gets.chomp
+  contador=1
+  a=0
+  while contador <= pila[:size]
+    if isbn == libro[:ISBN]
+      contador=pila[:size]+1
+      a=1
+    elsif libro [:siguiente]==nil&&libro[:ISBN]!=isbn
+      a=0
+    else
+      libro = libro[:siguiente]
+    end
+    contador+=1
+  end
+  if a==1
+    # limpiar_pantalla()
+    # user_table = table do |t|
+    #   t.headings = 'ISBN', 'Nombre del Libro', 'Autor', 'Precio', 'Existencias'
+    #   t << [
+    #     libro[:ISBN],
+    #     libro[:nombre],
+    #     libro[:autor],
+    #     libro[:Precio],
+    #     libro[:existencias]
+    #   ]
+    limpiar_pantalla()
+    tabla = Terminal::Table.new do |t|
+      t.headings = ['NOMBRE','AUTOR','ISBN','PRECIO','EXISTENCIAS']
+      t.add_row([
+        libro[:nombre],
+        libro[:autor],
+        libro[:ISBN],
+        libro[:Precio],
+        libro[:existencias]
+      ])
+    
+    puts user_table
+  else
+    limpiar_pantalla
+    puts 'El libro no se encuentra en el sistema'
+  end
+  gets
+end
+
+venta ={
+  tope:nil,
+  max:20,
+  size:0,
+  vacio:true,
+  final:nil
+}
+
+cola ={
+  vacio:true,
+  size:0,
+  max:5,
+  tope:nil,
+  final:nil
+}
+
+pila ={
+  tope:nil,
+  vacio:true,
+  size:0
+}
+
+begin
+  puts "\tBiblioteca de libros"
+  puts 'Listado de opciones: '
+  puts '1. Registro de Libros'
+  puts '2. Control de Ventas'
+  puts '3. Salir'
+  print 'Ingrese una opcion: '
+  opcion = gets.chomp
+  limpiar_pantalla()
+  if opcion == '1'
+    begin
+      puts "\tRegistro de Libros"
+      puts 'Listado de opciones: '
+      puts '1. Registro de nuevos Libros'
+      puts '2. Registro de autores'
+      puts '3. Lista de Libros'
+      puts '4. Lista de Autores'
+      puts '5. Buscar libro'
+      puts '6. Buscar Autor'
+      puts '7. Salir'
+      print 'Ingrese una opcion: '
+      opc_2 = gets.chomp
+      if opc_2 == '1'
+        # registro_libros(pila, cola)
+      elsif opc_2 == '2'
+        # registro_autores(cola)
+      elsif opc_2 == '3'
+        lista_libros(pila)
+      elsif opc_2 == '4'
+        # lista_autores(cola)
+      elsif opc_2 == '5'
+        funcion(pila)
+      elsif opc_2 == '6'
+        # buscar_autor(cola, pila)
+    end
+    limpiar_pantalla()
+    end while opc_2 != '7'
+  end
+ limpiar_pantalla()
+end while opcion != '3'
