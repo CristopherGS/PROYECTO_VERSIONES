5d3d4702229c4c5e7f84392386715d14bee2ca81
Donald Abac <ddaniel_abac@outlook.es>
2018-10-27 21:00:23 -0500

    reparacion

--- main.rb
+++ main.rb
@@ -5,7 +5,54 @@ def limpiar_pantalla
   system('clear')
 end
 
-def buscar_libro(pila)
+def buscar_libro(pila,isbn)
+  libro = pila[:tope]
+  contador=1
+  while contador<=pila[:size]
+    if libro[:ISBN]==isbn
+      contador=pila[:size]+1
+      return libro
+    elsif libro[:siguiente]==nil && libro[:ISBN]!=isbn
+      return 'El libro que ingreso no existe en el sistema'
+      break
+    else
+      nuevo_elemento = libro[:siguiente]
+      libro = nuevo_elemento
+      contador+=1
+    end
+  end
+end
+
+def lista_libros(pila)
+  limpiar_pantalla
+  if pila[:tope] == nil
+    puts 'No Hay Libros Ingresados O En Existencia'
+  else
+    user_table=table do |t|
+      t.title = '*-*-*-*-LISTA DE LIBROS-*-*-*-*'
+      t.headings = 'ISB', 'NOMBRE', 'PRECIO','AUTOR','EXISTENCIAS'
+      aux = pila[:tope]
+      loop do
+       t << [
+        aux[:ISBN],
+        aux[:nombre],
+        aux[:precio],
+        aux[:autor],
+        aux[:existencias]
+      ]
+       if aux[:siguiente] == nil
+          break
+       end
+       aux = aux[:siguiente]
+      end
+    end
+      puts user_table
+  end
+  gets()
+end
+
+def buscar_libro1(pila)
+  limpiar_pantalla
   libro = pila[:tope]
   print 'Ingrese el ISBN del Libro: '
    isbn = gets.chomp
@@ -25,6 +72,7 @@ def buscar_libro(pila)
   if a==1
     limpiar_pantalla
     user_table = table do |t|
+      t.title = '*-*-*-*-Buscador de Libros-*-*-*-*'
       t.headings = 'ISBN', 'Nombre del Libro', 'Autor', 'Precio', 'Existencias'
       t << [
         libro[:ISBN],
@@ -40,16 +88,16 @@ def buscar_libro(pila)
     puts 'El libro no se encuentra en el sistema'
   end
   gets()
-  end
 end
 
 venta ={
   tope:nil,
   max:20,
-  size:0
+  size:0,
   vacio:true,
   final:nil
 }
+
 cola ={
   vacio:true,
   size:0,
@@ -57,11 +105,13 @@ cola ={
   tope:nil,
   final:nil
 }
+
 pila ={
   tope:nil,
   vacio:true,
   size:0
 }
+
 begin
   puts "\tBiblioteca de lirbros"
   puts 'Listado de opciones: '
@@ -87,40 +137,36 @@ begin
       opcion1 = gets.chomp
       case opcion1
       when '1'
+        registro_nuevo_libro(plia,cola)
       when '2'
+        nuevo_autor(cola)
       when '3'
-        buscar_libro(pila)
+        buscar_libro1(pila)
       when '4'
+        mostrar_autor(cola,pila)
       when '5'
+        listado_de_libros(pila)
       when '6'
+        lista_libros(pila)
       end
     end while opcion1 != '7'
   when '2'
+    begin
+      puts "\tControl de Ventas"
+      puts 'Listado de opciones: '
+      puts '1. Registro de una Venta'
+      puts '2. Buscar una Venta'
+      puts '3. Listado de Ventas'
+      puts '4. Salir'
+      opcion2=gets.chomp
+      case opcion2
+      when '1'
+        registro_de_ventas(venta,pila)
+      when '2'
+        mostrar_una_venta(venta)
+      when '3'
+      end
+    end while opcion2 !='4'
   end
   limpiar_pantalla()
 end while opcion != '3'
-
-
-def listado_de_libros(pila)
-  if pila[:tope] == nil
-    puts 'No Hay Libros Ingresados O En Existencia'
-  else
-  tabla = Terminal::Table.new do |t|
-    t.title = 'Listado de Libros'
-    t.headings = ['ISB', 'NOMBRE', 'PRECIO','AUTOR','EXISTENCIAS']
-
-    aux = pila[:tope]
-    loop do
-      t.add_row([
-        aux[:ISBN],
-        aux[:nombre],
-        aux[:precio],
-        aux[:autor],
-        aux[:existencias]
-      ])
-      if aux[:siguiente] == nil
-        break
-      end
-      aux = aux[:siguiente]
-    end
-  end
